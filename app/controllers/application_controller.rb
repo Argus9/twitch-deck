08bb5874ed61a089040c1ec0611cfd301afe1a33
Jonathan Zisser <jzisser9@gmail.com>
2016-02-27 16:22:33 -0500

    Added rufus-scheduler, and scheduled two tasks, both to be run at 6 AM: Email a daily digest to myself, and delete all user accounts that are older than 30 days that haven't been activated yet.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,4 +3,27 @@ class ApplicationController < ActionController::Base
 	# For APIs, you may want to use :null_session instead.
 	protect_from_forgery with: :exception, except: :replace_main_stream
 	include SessionsHelper
+
+	scheduler = Rufus::Scheduler.start_new
+
+	# Run daily at 6 AM.
+	scheduler.cron '0 0 6 1/1 * ? *' do
+		# Compile and send a daily digest of stats.
+		yesterday = 1.day.ago.to_date
+		new_users = User.where('DATE(created_at) = ?', yesterday).count
+		total_users = User.count
+		activated_users = User.where('activated = true').count
+
+		streamers = new Hash 0
+		User.where('streamers is not null').select(:streamers).map(&:streamers).each do |streamers|
+			streamers.split('&').each { |streamer| streamers[streamer] += 1 }
+		end
+
+		top_streamers = streamers.sort_by { |_, count| -count }[0...10]
+
+		UserMailer.digest(top_streamers, new_users, total_users, activated_users, yesterday).deliver_now
+
+		# Cull inactive user accounts that are at least 30 days old.
+		User.where('created_at <= ? AND activated = false', 30.days.ago).destroy_all
+	end
 end
